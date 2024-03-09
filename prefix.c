3ec26c8ebd912a24a2f0ff7bc3778a760dcd6e95
fenil patel <126600325+fenil-ptl@users.noreply.github.com>
2024-03-09 18:39:01 +0530

    Add files via upload

---
+++ prefix.c
@@ -0,0 +1,85 @@
+# include<stdio.h> 
+# include<stdlib.h>
+# include<string.h>
+# include<ctype.h>
+
+# define SIZE 50
+
+void push(char *stack, int *top, char next);
+char pop(char *stack, int *top);
+ 
+int main()
+{
+
+    char exp[SIZE], stack[SIZE];
+    int top = -1;
+    printf("Enter string : ");
+    scanf("%s", exp);
+
+    // Only condition changes for evalution of prefix
+    for(int i = strlen(exp) - 1; i >= 0; i--)
+    {
+        // if there is operand push into stack
+        if(isdigit(exp[i]))
+        {
+            push(stack, &top, exp[i] - '0');
+        }
+        else
+        {
+            // If there is operator then pop two operands from stack and perform the operation between them
+            int val1 = pop(stack, &top);  
+            int val2 = pop(stack, &top);  
+
+            switch(exp[i])
+            {
+                case '+':
+                    push(stack, &top, val2 + val1);
+                    break;
+
+                case '-':
+                    push(stack, &top, val2 - val1);
+                    break;
+
+                case '*':
+                    push(stack, &top, val2 * val1);
+                    break;
+
+                case '/':
+                    push(stack, &top, val2 / val1);
+                    break;
+            }
+        }
+    }
+
+    int ans = pop(stack, &top);
+    printf("Evaluation of Prefix expression is : %d", ans);
+    return 0;
+}
+
+// Push Operation
+void push(char *stack, int *top, char next)
+{
+    if(*top == SIZE - 1)
+    {
+        printf("\nSTACK OVERFLOW\n");
+    }
+    else
+    {
+        *top = *top + 1;
+        stack[*top] = next;
+    }
+}
+
+// Pop Operation
+char pop(char *stack, int *top)
+{
+    if(*top == -1)
+    {
+        printf("\nSTACK UNDERFLOW\n");
+    }
+    else
+    {
+        *top = *top - 1;
+        return stack[*top + 1];
+    }
+}
